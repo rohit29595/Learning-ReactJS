ed0a61e32f008db2d19c87091968864d61829e94
rohit29595 <rk4447@rit.edu>
2020-12-15 02:54:54 -0500

    third commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
+import Message from './Components/Message'
+
 
 function App() {
   return (
     <div className="App">
     
-    <Greet/>
+    {/* <Greet name="Bruce" heroname = "Batman"> 
+    <p>This is children props</p></Greet>
+    <Greet name="Clark" heroname = "Superman">
+      <button>Action</button></Greet>
+    <Greet name="Diana" heroname = "Wonder Woman"/>
+
+    <Welcome name="Bruce" heroname = "Batman"/> 
+    <Welcome name="Clark" heroname = "Superman"/> 
+    <Welcome name="Diana" heroname = "Wonder Woman"/>  */}
+
+    <Message/>
+
+
+
+
+
     </div>
   );
 }
--- hello-world/src/Components/Greet.js
+++ hello-world/src/Components/Greet.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 
-const Greet = () => <h1>Hello Rohit !!</h1>
+const Greet = (props) => {
+    console.log(props.name)
+
+return (
+    <div>
+    <h1>Hello  {props.name} a.k.a {props.heroname}</h1>
+    {props.children}
+    </div>
+)
+}
 
 
 export default Greet
